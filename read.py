22ea354d53e63a7de475037a8164805035e29170
tjacek <tjacek@student.agh.edu.pl>
2016-10-21 23:44:21 +0200

    flatten array

--- read.py
+++ read.py
@@ -10,6 +10,13 @@ class Knn(object):
         k_metric=[ self.metric(new_vec_i)
     	           for vec_i in vectors]
 
-def read_mat(name):
+def read_mat(name,flat=False):
     mat_contents = sio.loadmat(name)
-    return mat_contents['a']
+    mat_array=mat_contents['a']
+    if(flat):
+        return mat_array.flatten()
+    else:
+        return mat_array
+
+if __name__ == "__main__":
+    read_mat(name)
