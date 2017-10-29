8d309ff8b2167c24e5d46e4ae0ecbe946741224e
tjacek <tjacek@student.agh.edu.pl>
2017-10-29 18:38:29 -0500

    decision boundary

--- exp.py
+++ exp.py
@@ -2,19 +2,23 @@ import gen
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+import knn
 
 class Experiment(object):
     def __init__(self,gen,model):
         self.gen=gen
         self.model=model
 
-    def __call__(self,n=500):
+    def __call__(self,n=500,show=False):
         dataset=self.gen(n)
         train,test=self.split(dataset)
         self.model.train(train)
         y_pred=self.pred(test)
         y_true=test.y
         print(classification_report(y_true, y_pred,digits=4))
+        if(show):
+#           dataset.show()
+            knn.decision_boundary(dataset,self.model.clf)
 
     def pred(self,dataset):
     	return [ self.model(x_i) for x_i in dataset.X]
--- knn.py
+++ knn.py
@@ -1,34 +1,77 @@
-import numpy as np 
-from collections import Counter
-
-def euclid(a,b):
-    return np.linalg.norm(a-b)
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
 
 class KNN(object):
-    def __init__(self,k=1,metric=euclid):
-        self.model=None#train
-        self.metric=metric
+    def __init__(self,k=1):
         self.k=k
-
-    def __call__(self,sample):
-        inds=self.distance(sample)
-        nearest=self.get_cats(inds)
-        count =Counter(nearest)
-        pred_cat=count.most_common()[0][0]
-        return pred_cat
+        self.clf = neighbors.KNeighborsClassifier(k)
 
     def train(self,dataset):
-        self.model=dataset
+        self.clf.fit(dataset.X,dataset.y)
 
-    def get_cats(self,inds):
-        return [self.model.y[i] for i in inds ]
+    def __call__(self,sample):
+        pred=self.clf.predict(sample)#np.c_[xx.ravel(), yy.ravel()])
+        print(type(pred))
+        print(pred)
+        return pred[0]
 
-    def distance(self,sample,k=None):
-        print(len(self.model))
-        dists=[ self.metric(sample,train_i) 
-                  for train_i in self.model.X]
-        dists=np.array(dists)
-        dist_inds=dists.argsort()
-        if(self.k!=None):
-            return dist_inds[0:self.k]	
-        return dist_inds
+def decision_boundary(dataset,clf,h=0.02):
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+    
+    X=np.array(dataset.X)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    Z=clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    plt.scatter(X[:, 0], X[:, 1], c=dataset.y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+#    plt.title("3-Class classification (k = %i, weights = '%s')"
+#              % (n_neighbors, weights))
+
+    plt.show()
+
+#h = .02  # step size in the mesh
+
+# Create color maps
+#cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+#cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+#for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    #clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    #clf.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+#    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+#    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+#    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+#                         np.arange(y_min, y_max, h))
+#    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+#    Z = Z.reshape(xx.shape)
+#    plt.figure()
+#    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+#    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+#                edgecolor='k', s=20)
+#    plt.xlim(xx.min(), xx.max())
+#    plt.ylim(yy.min(), yy.max())
+#    plt.title("3-Class classification (k = %i, weights = '%s')"
+#              % (n_neighbors, weights))
+
+#plt.show()
