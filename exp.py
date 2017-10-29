b205f5ee2e1de38bbf3b17a09a81695ed5229806
tjacek <tjacek@student.agh.edu.pl>
2017-10-29 14:14:27 -0500

    experiment module

---
+++ exp.py
@@ -0,0 +1,25 @@
+import gen
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+class Experiment(object):
+    def __init__(self,gen,model):
+        self.gen=gen
+        self.model=model
+
+    def __call__(self,n=500):
+        dataset=self.gen(n)
+        train,test=self.split(dataset)
+        self.model.train(train)
+        y_pred=self.pred(test)
+        y_true=test.y
+        print(classification_report(y_true, y_pred,digits=4))
+
+    def pred(self,dataset):
+    	return [ self.model(x_i) for x_i in dataset.X]
+
+    def split(self,dataset):
+        train=dataset.select(gen.Modulo(0))
+        test=dataset.select(gen.Modulo(1))
+        return train,test
--- knn.py
+++ knn.py
@@ -1,26 +1,34 @@
 import numpy as np 
+from collections import Counter
+
+def euclid(a,b):
+    return np.linalg.norm(a-b)
 
 class KNN(object):
-	def __init__(self,train,cats,metric,k=1):
-		self.train=train
-		self.metric=metric
-		self.k=k
+    def __init__(self,k=1,metric=euclid):
+        self.model=None#train
+        self.metric=metric
+        self.k=k
 
     def __call__(self,sample):
-    	inds=self.distance(sample)
+        inds=self.distance(sample)
         nearest=self.get_cats(inds)
         count =Counter(nearest)
         pred_cat=count.most_common()[0][0]
         return pred_cat
 
-    def get_cats(self,inds):
-        return [self.train.y[i] for i in inds ]
+    def train(self,dataset):
+        self.model=dataset
 
-    def distance(sample,k=None):
+    def get_cats(self,inds):
+        return [self.model.y[i] for i in inds ]
+
+    def distance(self,sample,k=None):
+        print(len(self.model))
         dists=[ self.metric(sample,train_i) 
-                  for train_i in self.train.X]
+                  for train_i in self.model.X]
         dists=np.array(dists)
-        dist_inds=distance.argsort()
+        dist_inds=dists.argsort()
         if(self.k!=None):
             return dist_inds[0:self.k]	
         return dist_inds
