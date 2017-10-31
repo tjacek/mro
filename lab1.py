0064ededcca67515ffcde3969c6282daea6ec85d
tjacek <tjacek@student.agh.edu.pl>
2017-10-30 21:01:59 -0700

    remove unused module

---
+++ lab1.py
@@ -0,0 +1,21 @@
+import gen
+import gen.ellipse,gen.preds
+import exp
+import knn
+
+def make_gen():
+    dist=gen.NormalDist()	
+    preds=[get_non_convex(),gen.ellipse.Ellipse(0.9,2.5)]
+    my_gen=gen.GenerateDataset(dist,preds)
+    return my_gen
+
+def get_non_convex():
+    return gen.preds.SimpleNonConvex([1.0,1.0],[-1.0,1.0],[0.0,0.0],[0.0,-1.0])
+
+metr=['mahalo','lmnn']
+my_gen=make_gen()
+exp1=exp.Experiment(my_gen,knn.KNN(k=1,metric='lmnn'))#'mahalo'))
+exp1(250,True)
+
+#dataset=my_gen(500)
+#dataset.show()
