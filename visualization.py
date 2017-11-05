b87d62a3c3065e57ee289d394c1a794ca37b94b6
tjacek <tjacek@student.agh.edu.pl>
2017-11-05 05:03:17 -0800

    visualization module

---
+++ visualization.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+def decision_boundary(dataset,clf,h=0.02):
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+    
+    X=np.array(dataset.X)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    Z=clf(np.c_[xx.ravel(), yy.ravel()],single=False)
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    plt.scatter(X[:, 0], X[:, 1], c=dataset.y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+#    plt.title("3-Class classification (k = %i, weights = '%s')"
+#              % (n_neighbors, weights))
+
+    plt.show()
