e921c8fc537096a0372327a21cefefcd0f4d59d9
tjacek <tjacek@student.agh.edu.pl>
2017-11-06 15:50:44 -0800

    radius pred

--- gen/conic.py
+++ gen/conic.py
@@ -1,8 +1,7 @@
 import numpy as numpy
 
-
 class Hiperbola(object):
-    def __init__(self,a,b):
+    def __init__(self,a=1.0,b=1.0):
         self.a=a
         self.b=b
 
@@ -22,3 +21,15 @@ class Ellipse(object):
         x=(p[0]-self.c[0])/self.a
         y=(p[1]-self.c[1])/self.b
         return (x**2 + y**2) < self.r
+
+class Radius(object):
+    def __init__(self,r1,r2):
+        self.r1=r1
+        self.r2=r2
+
+    def __call__(self,p):
+        r_p=r(p)
+        return self.r1<r_p and self.r1>r_p
+
+def r(p):
+    return np.sqrt(p[0]**2+p[1]**2)
