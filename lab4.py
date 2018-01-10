124cd1f4428f3c6efeb1e24a4ffbf42faadf2469
tjacek <tjacek@student.agh.edu.pl>
2018-01-10 16:35:36 -0700

    cluster experiment

--- lab4.py
+++ lab4.py
@@ -3,6 +3,16 @@ import gen,gen.dists
 import cluster
 import visualization
 
+def cluster_experiment(init,n_iters=100,n_points=500):
+    clust_gen=get_cluster_generator()
+    basic_data=clust_gen(n_points)
+    cls_alg=cluster.KMeans(init)
+    cls_alg.start(basic_data)
+    for i in range(n_iters):
+        cls_alg()
+    cls_data=cls_alg.get_result()
+    visualization.show(cls_data,legend=False)
+
 def get_cluster_generator(sigma=0.5,n=3,step=5.0):
     x=[ (i+1)*step for i in xrange(n) ]
     mu=[ (x_i,x_j)  
@@ -13,11 +23,5 @@ def get_cluster_generator(sigma=0.5,n=3,step=5.0):
     clust_gen=gen.dists.CompositeDist(dists)
     return  gen.GenerateDataset(clust_gen)
 
-clust_gen=get_cluster_generator()
-basic_data=clust_gen(500)
-cls_alg=cluster.KMeans(cluster.uniform_init)
-cls_alg.start(basic_data)
-for i in range(100):
-    cls_alg()
-cls_data=cls_alg.get_result()
-visualization.show(cls_data,legend=False)
+
+cluster_experiment(init=cluster.uniform_init)
