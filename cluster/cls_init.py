2ea80814728d20696e39b1f08d13ad5fbd8b6719
tjacek <tjacek@student.agh.edu.pl>
2018-01-11 19:47:29 -0700

    kmeans plus

--- cluster/cls_init.py
+++ cluster/cls_init.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import random
+import cluster
 
 def uniform_init(points,k):
     points=np.array(points)
@@ -10,7 +11,6 @@ def uniform_init(points,k):
 
 def forgy_init(points,k):
     random.shuffle(points)
-    print(points[:k])
     return points[0:k]
 
 def partition_init(points,k):
@@ -23,5 +23,22 @@ def partition_init(points,k):
                         if random_cls[j]==i]
         return np.array(cluster_i)
     return [ np.mean(cls_helper(i),axis=0)
-                for i in range(k)]	
-    	
+                for i in range(k)]
+
+def kmeans_plus(points,k):
+    n_points=len(points)
+    start_index=np.random.randint(n_points)
+    means=[points[start_index]]
+    indexes=[i for i in range(n_points)]
+    def nearest_cluster(point_i):
+        print(len(means))
+        return min([ cluster.L2(point_i-mean_j)**2
+                        for mean_j in means])
+    def next_mean():
+        weights=[ nearest_cluster(point_i) for point_i in points]
+    	weights=np.array(weights)/sum(weights)
+        mean_index=np.random.choice(indexes, 1, p=weights)
+        return points[mean_index]
+    for i in range(1,k):
+        means.append(next_mean())
+    return means
