eb1aa900f1e3f48e0e97182505a7ea4b601ed9b0
tjacek <tjacek@student.agh.edu.pl>
2018-01-16 13:34:20 +0100

    add Clustering Experiment class

--- lab4.py
+++ lab4.py
@@ -1,19 +1,27 @@
 import numpy as np
 import gen,gen.dists
 import cluster
+import cluster.quality
+import cluster.cls_init as cls_init
 import visualization
 
-def cluster_experiment(init,n_iters=100,n_points=500):
-    clust_gen=get_cluster_generator()
-    basic_data=clust_gen(n_points)
-    cls_alg=cluster.KMeans(init)
-    cls_alg.start(basic_data)
-    for i in range(n_iters):
-        cls_alg()
-    cls_data=cls_alg.get_result()
-    visualization.show(cls_data,legend=False)
+class ClusterExperiment(object):
+    def __init__(self,cls_alg,cls_quality):
+        self.cls_gen=get_cluster_generator()
+        self.cls_alg=cls_alg
+        self.cls_quality=cls_quality
 
-def get_cluster_generator(sigma=0.5,n=3,step=5.0):
+    def __call__(self,n_iters=500,n_points=500):
+        basic_data=self.cls_gen(n_points)
+        cls_alg.start(basic_data)
+        for i in range(n_iters):
+            cls_alg()
+        clustering=cls_alg.get_clustering()
+        print(self.cls_quality(clustering))
+        cls_data=cls_alg.get_result()
+        visualization.show(cls_data,legend=False)
+
+def get_cluster_generator(sigma=0.3,n=3,step=5.0):
     x=[ (i+1)*step for i in xrange(n) ]
     mu=[ (x_i,x_j)  
          for x_i in x
@@ -23,5 +31,6 @@ def get_cluster_generator(sigma=0.5,n=3,step=5.0):
     clust_gen=gen.dists.CompositeDist(dists)
     return  gen.GenerateDataset(clust_gen)
 
-
-cluster_experiment(init=cluster.uniform_init)
+cls_alg=cluster.KMeans(cls_init.kmeans_plus)
+cls_exp=ClusterExperiment(cls_alg, cluster.quality.DBI_quality)
+cls_exp()
