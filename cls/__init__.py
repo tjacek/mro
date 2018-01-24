8fd01e0fff188542aaff7aa26168788c1170e56f
tjacek <tjacek@student.agh.edu.pl>
2018-01-23 22:13:29 -0600

    svm

--- cls/__init__.py
+++ cls/__init__.py
@@ -1,8 +1,9 @@
 import numpy as np 
+from sklearn import svm
 
 class Cls(object):
-    def __init__(self,model):
-        self.model=model
+    def __init__(self,C):
+        self.model= svm.SVC(C=1.0,decision_function_shape='ovo')
 
     def __call__(self,dataset):
         y_pred=self.clf.predict(dataset.X)
